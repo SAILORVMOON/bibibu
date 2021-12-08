92cbe438d39cffce128d1ff4dda9b51e362125e1
lexXXXey <lesha.nevskiy.05@mail.ru>
2021-12-08 18:15:47 +0500

    3

--- app/src/main/java/com/example/bibibu/MainActivity.java
+++ app/src/main/java/com/example/bibibu/MainActivity.java
@@ -3,45 +3,71 @@ package com.example.bibibu;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class MainActivity extends Activity implements View.OnTouchListener {
+import java.util.ArrayList;
 
-    float x, y;
-    Path path;
+public class MainActivity extends Activity {
+
+    ArrayList<Path> paths;
     Paint p;
+    Path path;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(new MyDraw(this));
+        path = new Path();
+        paths = new ArrayList<>();
     }
 
-    class MyDraw extends View{
+    class MyDraw extends View {
         public MyDraw(Context context) {
             super(context);
             p = new Paint();
+            p.setColor(Color.BLACK);
             p.setStrokeWidth(3);
             p.setStyle(Paint.Style.STROKE);
+
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawCircle(50, 50, 10, p);
+            for (int i =0; i< paths.size(); i++){
+                canvas.drawPath(paths.get(i), p);
+                invalidate();
+            }
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent motionEvent) {
+            float x = motionEvent.getX();
+            float y = motionEvent.getY();
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    path.moveTo(x, y);
+                    paths.add(path);
+                    invalidate();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    path.lineTo(x, y);
+                    paths.add(path);
+                    invalidate();
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                path.moveTo(x = motionEvent.getX(), y = motionEvent.getX());
-                break;
-            case MotionEvent.ACTION_MOVE:
-                path.lineTo(x = motionEvent.getX(), y = motionEvent.getX());
-                break;
-            default:
-                break;
-        }
-        return false;
-    }
+
+
 }
